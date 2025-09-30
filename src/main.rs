2fc86326372e5f081bf6f10c62920f23fc4400f3
Wade Cheng <86323079+wade-cheng@users.noreply.github.com>
2025-09-30 00:13:11 -0400

    feat: swap to bm25 (#6)

---
+++ src/main.rs
@@ -0,0 +1,100 @@
+//! Search cmucourses with bm25 crate.
+//!
+//! Try with `cargo run --release path/to/db/json`, then interactively submit queries.
+//!
+//! The database json file is named `courses.json` in https://scottylabs.slack.com/files/U08M22PL413/F09G6PQPXAP/course-search-sandbox.zip.
+
+use bm25::{SearchEngineBuilder, Tokenizer};
+use std::{fs, io::Write, time::Instant};
+
+/// n-wide sliding windows over a str.
+///
+/// From https://stackoverflow.com/questions/51257304/.
+fn char_windows<'a>(src: &'a str, win_size: usize) -> impl Iterator<Item = &'a str> {
+    src.char_indices().flat_map(move |(from, _)| {
+        src[from..]
+            .char_indices()
+            .skip(win_size - 1)
+            .next()
+            .map(|(to, c)| &src[from..from + to + c.len_utf8()])
+    })
+}
+
+/// Tokenize with n-grams.
+///
+/// Change how wide the n-gram is with `N` in the source code.
+///
+/// This could be a const generic but I don't want to use too much magic.
+#[derive(Default)]
+struct NGramTokenizer;
+
+impl Tokenizer for NGramTokenizer {
+    fn tokenize(&self, input_text: &str) -> Vec<String> {
+        const N: usize = 3;
+
+        char_windows(input_text, N)
+            .map(|window| window.to_string())
+            .collect()
+    }
+}
+
+fn main() {
+    let mut args = std::env::args().skip(1);
+    let path_to_db_json = args.next().unwrap();
+
+    let db_json_file = fs::read(&path_to_db_json).expect(&format!(
+        "COULD NOT READ DATABASE JSON FILE {}. error was",
+        path_to_db_json
+    ));
+
+    let db = json::parse(str::from_utf8(&db_json_file).unwrap()).unwrap();
+
+    println!("db json has {} course entries", db.len());
+    println!("starting index-building step");
+
+    let search_engine = SearchEngineBuilder::<u32, u32, NGramTokenizer>::with_tokenizer_and_corpus(
+        NGramTokenizer {},
+        db.entries().map(|entry| {
+            format!(
+                "{} | {} | {}",
+                entry.1["courseID"].to_string(),
+                entry.1["name"].to_string(),
+                entry.1["desc"].to_string()
+            )
+        }),
+    )
+    .build();
+
+    println!("done building index. moving to query step.\n");
+
+    let mut buffer = String::new();
+    loop {
+        // print user prompt
+
+        print!("query > ");
+        std::io::stdout().flush().unwrap();
+
+        // get user input
+        buffer.clear();
+        std::io::stdin().read_line(&mut buffer).unwrap();
+
+        /// ANSI control code to clear the terminal screen
+        const CLEAR: &str = "\x1b[2J";
+        print!("{CLEAR}");
+
+        // search and record duration it took
+        let time_before_searching = Instant::now();
+
+        let results = search_engine.search(&buffer, 7);
+
+        println!(
+            "\n\n---QUERY \"{}\" RETURNED COURSES ORDERING IN {} SECONDS:",
+            &buffer[..buffer.len() - 1],
+            time_before_searching.elapsed().as_secs_f64()
+        );
+
+        for result in results {
+            println!("{}\n\n", result.document.contents);
+        }
+    }
+}
